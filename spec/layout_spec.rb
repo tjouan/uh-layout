b6a1f580e9114297bf4ed8304ce4c26205f9feb7
Thibault Jouan <tj@a13.fr>
2014-11-27 08:51:08 +0000

    Add Layout#<< specs

--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -3,12 +3,34 @@ require 'layout'
 module Holo
   describe Layout do
     let(:geo)         { Geo.new(0, 0, 640, 480) }
+    let(:client)      { instance_spy WM::Client }
     subject(:layout)  { described_class.new }
+
+    before { layout.screens = { 0 => geo } }
 
     describe '#screens=' do
       it 'assigns given screens as Screen objects in a Container' do
-        layout.screens = { 0 => geo }
         expect(layout.screens.entries).to eq [Layout::Screen.new(0, geo)]
+      end
+    end
+
+    describe '#<<' do
+      before { layout << client }
+
+      it 'assigns suggested geo to given client' do
+        expect(client).to have_received(:geo=).with(layout.suggest_geo_for :window)
+      end
+
+      it 'moveresizes given client' do
+        expect(client).to have_received :moveresize
+      end
+
+      it 'shows given client' do
+        expect(client).to have_received :show
+      end
+
+      it 'focuses given client' do
+        expect(client).to have_received :focus
       end
     end
   end
