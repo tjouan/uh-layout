d18a889bfe9c47c9cdd9daac081eabd80df61e3f
Thibault Jouan <tj@a13.fr>
2014-11-27 04:08:02 +0000

    Implement layout callbacks

--- lib/layout.rb
+++ lib/layout.rb
@@ -1,2 +1,17 @@
 class Layout
+  include Holo
+
+  def suggest_geo_for(window)
+    Geo.new(0, 0, 320, 240)
+  end
+
+  def <<(client)
+    client.geo = Geo.new(0, 0, 320, 240)
+    client.moveresize
+    client.show
+    client.focus
+  end
+
+  def remove(client)
+  end
 end
