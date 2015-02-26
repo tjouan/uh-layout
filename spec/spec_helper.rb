c083f5eac1badfe27b4875d0203c1369567a7941
Thibault Jouan <tj@a13.fr>
2015-02-26 09:23:11 +0000

    Configure rspec-mocks strict double verification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,10 @@ require 'holo'
 require 'holo/wm'
 
 Dir['spec/support/**/*.rb'].map { |e| require e.gsub 'spec/', '' }
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+    mocks.verify_partial_doubles = true
+  end
+end
