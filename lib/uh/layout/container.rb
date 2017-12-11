74f878fd26dd6957619e9a682681349bffc85c92
Thibault Jouan <tj@a13.fr>
2017-12-11 02:11:26 +0000

    Fix coding standards in Container#remove_entry

--- lib/uh/layout/container.rb
+++ lib/uh/layout/container.rb
@@ -67,7 +67,7 @@ module Uh
 
       def remove_entry entry
         fail ArgumentError, 'unknown entry' unless include? entry
-        @entries.delete_at (index = @entries.index(entry))
+        @entries.delete_at (index = @entries.index entry)
         if @current_index != 0 && @current_index > index
           @current_index -= 1
         end
